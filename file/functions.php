07349fca2d7999276d65962759a8cfd6f59f22c9
yoichi kobayashi <info@tplh.net>
2016-12-15 14:52:32 +0900

    [edit] function.php about thumbnail.

--- file/functions.php
+++ file/functions.php
@@ -51,6 +51,9 @@
   add_theme_support('post-thumbnails');
   add_image_size('thumb-name', 700, 420, true);
 
+  // サムネイルにsrcsetが自動付与されてしまうのを解除
+  add_filter( 'max_srcset_image_width', create_function( '', 'return 1;' ) );
+
   // 固定ページに概要を追加
   add_post_type_support( 'page', 'excerpt' );
 
