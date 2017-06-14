d70563d3c6548e192246f156e4c74560344aa86d
yoichi kobayashi <info@tplh.net>
2017-06-15 00:04:17 +0900

    [add] var about Smart Custom Fields.

--- file/taxonomy.php
+++ file/taxonomy.php
@@ -1,11 +1,12 @@
 <?php
-  $tax_name = 'tax_news';
-  $term_data = get_term_by('slug', $term, $tax_name);
-  $meta_path = get_term_link($term, $tax_name);
+  $taxonomy = 'tax_news';
+  $term_data = get_term_by('slug', $term, $taxonomy);
+  $meta_path = get_term_link($term, $taxonomy);
   $meta_title = $term_data->name. 'カテゴリの記事一覧';
   $meta_description = $term_data->description;
   $meta_keywords = '';
   $og_type = 'website';
+  $cf = SCF::get_term_meta( $term_data->term_id, $taxonomy ); // Smart Custom Fieldsを使う場合は指定
 
   include 'header.php';
 ?>
