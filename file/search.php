65069f253e75d7738c6a477a3a1e53e83047ac32
yoichi kobayashi <info@tplh.net>
2017-10-27 16:15:37 +0900

    [add] create a new theme file for search result.

---
+++ file/search.php
@@ -0,0 +1,15 @@
+<?php
+  global $wp_query;
+  $keyword_search = $wp_query->query['s'];
+  $meta_path = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+  $meta_title = '「'. $keyword_search. '」の検索結果';
+  $meta_description = '「'. $keyword_search. '」の検索結果を表示しています。';
+  $meta_keywords = '';
+  $meta_ogp_type = 'website';
+
+  include 'header.php';
+?>
+
+<?php
+  get_footer();
+?>
