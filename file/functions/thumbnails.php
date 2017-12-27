3794687d0cac3baf6e674d429b23fa14603b52c7
yoichi kobayashi <info@tplh.net>
2017-12-27 17:32:47 +0900

    clean up a process to add thumbnail.

--- file/functions/thumbnails.php
+++ file/functions/thumbnails.php
@@ -1,7 +1,7 @@
 <?php
   // サムネイルサイズを定義
   add_theme_support( 'post-thumbnails' );
-  add_image_size( 'thumb-adjacent', 300, 210, true );
+  add_image_size( 'thumb-name', 700, 420, true );
 
   // サムネイルにsrcsetが自動付与されてしまうのを解除
   add_filter( 'max_srcset_image_width', create_function( '', 'return 1;' ) );
