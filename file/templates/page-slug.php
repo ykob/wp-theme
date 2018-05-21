42426a67322190abb360ec103ce1de9c33b9d663
yoichi kobayashi <info@tplh.net>
2018-05-21 19:03:56 +0900

    clean up.

--- file/templates/page-slug.php
+++ file/templates/page-slug.php
@@ -3,7 +3,7 @@
 Template Name: Page Template
 */
   // pタグが自動挿入されるのを防ぐ
-  remove_filter('the_content', 'wpautop');
+  remove_filter( 'the_content', 'wpautop' );
   remove_filter( 'the_excerpt', 'wpautop' );
 
   // Smart Custom Fieldsのデータを取得
@@ -15,10 +15,6 @@ Template Name: Page Template
   $meta_description = get_the_excerpt();
   $meta_keywords = '';
   $meta_ogp_type = 'website';
-
-  // 本文情報を取得
-  $post = get_page($page_id);
-  $page_id = $post->post_name;
 
   include(TEMPLATEPATH. '/header.php');
 ?>
