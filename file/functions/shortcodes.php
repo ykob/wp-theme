9b2c502e2cd9a8cd932a4fefeecb82905a24b59c
yoichi kobayashi <info@tplh.net>
2018-05-22 19:26:57 +0900

    added a new shortcode that has a role to get posts.

--- file/functions/shortcodes.php
+++ file/functions/shortcodes.php
@@ -11,4 +11,28 @@
     return ob_get_clean();
   }
   add_shortcode('include_myphp', 'include_myphp_func');
+
+  // 記事一覧を生成する
+  function get_posts_func( $args ) {
+    global $post;
+
+    extract( $args );
+    ob_start();
+
+    $posts_args = array(
+      'post_type' => array( 'post' ),
+      'posts_per_page' => 3,
+    );
+    $posts_array = get_posts( $posts_args );
+    $posts_html = '';
+
+    if ( count( $posts_array ) > 0 ) {
+      foreach ( $posts_array as $index => $post ) {
+        // 個別記事の処理をここに記載する
+      }
+    }
+    ob_end_clean();
+    return $posts_html;
+  }
+  add_shortcode( 'get_posts', 'get_posts_func' );
 ?>
