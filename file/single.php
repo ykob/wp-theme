3b33b31b299141b02a845230ca687219adc7e966
yoichi kobayashi <info@tplh.net>
2018-07-17 16:41:57 +0900

    divided meta title variable in single page theme file into two.

--- file/single.php
+++ file/single.php
@@ -5,11 +5,12 @@
   // Smart Custom Fieldsを使う場合は指定
   $cf = SCF::gets();
 
+  $post_title = get_the_title();
   $kv_array = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumnail-name', false );
   $kv_src = ( $kv_array[0] ) ? $kv_array[0] : '/img/common/thumb_default.jpg';
 
   $meta_path = get_the_permalink();
-  $meta_title = get_the_title();
+  $meta_title = $post_title;
   $meta_description = get_the_excerpt();
   $meta_keywords = '';
   $meta_ogp_type = 'article';
