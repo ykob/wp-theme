6f10e76bdfe0be74691123428de54ff95802e40e
yoichi kobayashi <info@tplh.net>
2018-07-19 21:43:01 +0900

    returned vars in a template file.

--- file/templates/page-slug.php
+++ file/templates/page-slug.php
@@ -2,6 +2,24 @@
 /*
 Template Name: Page Template
 */
+
+  // pタグが自動挿入されるのを防ぐ
+  remove_filter( 'the_content', 'wpautop' );
+  remove_filter( 'the_excerpt', 'wpautop' );
+
+  // Smart Custom Fieldsを使う場合は指定
+  $cf = SCF::gets();
+
+  // ページの固有変数を定義
+  $page_id = $cf['page_id'];
+  $page_title = get_the_title();
+
+  // メタ情報を定義
+  $meta_path = get_the_permalink();
+  $meta_title = $page_title;
+  $meta_description = get_the_excerpt();
+  $meta_keywords = '';
+
   include( TEMPLATEPATH. '/header.php' );
 ?>
 
