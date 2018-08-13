f748e70ed38ea3d5b892271ee071cb15a887238f
yoichi kobayashi <info@tplh.net>
2018-08-13 16:24:23 +0900

    change how to set a website domain in a header theme.

--- file/header.php
+++ file/header.php
@@ -1,6 +1,8 @@
 <?php
   $template_url = get_bloginfo( 'template_url' );
-  $website_domain = get_bloginfo( 'url' );
+  $blog_url = get_bloginfo( 'url' );
+  $parse_url = parse_url( $blog_url );
+  $website_domain = $parse_url['scheme']. '://'. $parse_url['host']. '/';
   $website_name = '';
   $meta_title_merge = ( $meta_title ) ? $meta_title. ' | '. $website_name : $website_name;
   $meta_keywords_base = '';
